e09bf49df03563ff5e4c8538f3cd6d3e3557cdbe
Roshilv3 <roshil.verma.3@gmail.com>
2024-02-19 05:05:34 +0530

    setup file

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open("README.md", 'r', encoding="utf-8") as f:
 
 
 setup(
-    name = "Wine Quality ML Project",
+    name = "Text Classification ML Project",
     version = "0.0.1",
     description = "This is the full ML Classification Project",
     long_description=long_description,
     author = "Roshil Verma",
     author_email = "roshil.verma.3@gmail.com",
-    url = "https://github.com/Roshilv3/Wine_Quality_ML",
+    url = "https://github.com/Roshilv3/Text_Classification_ML_NLP",
     packages = find_packages("requirements.txt")
 )
+
